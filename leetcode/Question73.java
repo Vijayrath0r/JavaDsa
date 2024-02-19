4a029ea8a6d49e9202eda09c428fe93383428b09
Vijay Rathor <vijay.rathor@mindruby.com>
2024-02-19 16:13:35 +0530

    Solved zero matrix problem on leetcode and local.

--- leetcode/Question73.java
+++ leetcode/Question73.java
@@ -1,20 +1,10 @@
-package Striver;
+package leetcode;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class SetMatrixZero {
-    public static void main(String[] args) {
-        // int[][] matrix = {
-        // { 1, 1, 1 },
-        // { 1, 0, 1 },
-        // { 1, 1, 1 }
-        // };
-        int[][] matrix = {
-                { 0, 1, 2, 0 },
-                { 3, 4, 5, 2 },
-                { 1, 3, 1, 5 }
-        };
+public class Question73 {
+    public static void setZeroes(int[][] matrix) {
         List<Integer> rows = new ArrayList<Integer>();
         List<Integer> cols = new ArrayList<Integer>();
 
@@ -33,13 +23,19 @@ public class SetMatrixZero {
 
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
-                if (rows.contains(i)||cols.contains(j)) {
-                    System.out.print("0 ");
-                } else {
-                    System.out.print(matrix[i][j]+" ");
+                if (rows.contains(i) || cols.contains(j)) {
+                    matrix[i][j] = 0;
                 }
             }
-            System.out.println();
         }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                { 0, 1, 2, 0 },
+                { 3, 4, 5, 2 },
+                { 1, 3, 1, 5 }
+        };
+        setZeroes(matrix);
     }
 }
