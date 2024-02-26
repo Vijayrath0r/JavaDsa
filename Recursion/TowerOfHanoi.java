b4a0c49337df4b7ff4e58e1e1d4118e384e48cb0
Vijay Rathor <vijay.rathor@mindruby.com>
2024-02-26 11:58:14 +0530

    Solved TowerOfHanoi with recursion.

---
+++ Recursion/TowerOfHanoi.java
@@ -0,0 +1,27 @@
+package Recursion;
+
+/**
+ * TowerOfHanoi
+ */
+public class TowerOfHanoi {
+    public static int count = 0;
+
+    public static void printTowerOfHanoi(int n, String source, String helper, String destination) {
+        if (n == 1) {
+            System.out.println("Trasfer disk   " + n + " From " + source + " to " + destination);
+            count++;
+            return;
+        }
+        count++;
+        printTowerOfHanoi(n - 1, source, destination, helper);
+        System.out.println("Trasfer disk   " + n + " From " + source + " to " + destination);
+        printTowerOfHanoi(n - 1, helper, destination, source);
+    }
+
+    public static void main(String[] args) {
+        int n = 3; // Bigger disk number bigger disk size.
+        printTowerOfHanoi(n, "Source", "Helper", "Destination");
+        System.out.println("steps taken - " + count);
+
+    }
+}
