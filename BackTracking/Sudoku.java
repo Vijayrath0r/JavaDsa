396c6144e179d0c8bfe9b34438f684304df4a54b
Vijay Rathor <vijay.rathor@mindruby.com>
2024-03-06 12:26:13 +0530

    Solved sudoku solver question

---
+++ BackTracking/Sudoku.java
@@ -0,0 +1,82 @@
+package BackTracking;
+
+public class Sudoku {
+    public static void printSudoku(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static boolean isSafe(char[][] board, int row, int col, int number) {
+        for (int i = 0; i < board.length; i++) {
+            if (board[row][i] == (char) (number + '0') || board[i][col] == (char) (number + '0')) {
+                return false;
+            }
+        }
+        int startRow = (row / 3) * 3;
+        int startCol = (col / 3) * 3;
+
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+                if (board[i][j] == (char) (number + '0')) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean solveSudoku(char[][] board, int row, int col) {
+        if (row == board.length || col == board.length) {
+            printSudoku(board);
+            return true;
+        }
+        int newRow;
+        int newCol;
+        if (row >= board.length - 1) {
+            newRow = 0;
+            newCol = col + 1;
+        } else {
+            newRow = row + 1;
+            newCol = col;
+        }
+        if (board[row][col] != '.') {
+            if (solveSudoku(board, newRow, newCol)) {
+                return true;
+            }
+        } else {
+            for (int i = 1; i <= 9; i++) {
+                if (isSafe(board, row, col, i)) {
+                    board[row][col] = (char) (i + '0');
+                    if (solveSudoku(board, newRow, newCol)) {
+                        return true;
+                    } else {
+                        board[row][col] = '.';
+                    }
+                } else {
+                    board[row][col] = '.';
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        char[][] board = {
+                { '5', '3', '.', '.', '7', '.', '.', '.', '.' },
+                { '6', '.', '.', '1', '9', '5', '.', '.', '.' },
+                { '.', '9', '8', '.', '.', '.', '.', '6', '.' },
+                { '8', '.', '.', '.', '6', '.', '.', '.', '3' },
+                { '4', '.', '.', '8', '.', '3', '.', '.', '1' },
+                { '7', '.', '.', '.', '2', '.', '.', '.', '6' },
+                { '.', '6', '.', '.', '.', '.', '2', '8', '.' },
+                { '.', '.', '.', '4', '1', '9', '.', '.', '5' },
+                { '.', '.', '.', '.', '8', '.', '.', '7', '9' }
+        };
+        solveSudoku(board, 0, 0);
+    }
+}
