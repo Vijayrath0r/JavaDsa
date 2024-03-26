36034f4c7a9cf4ed1789c659d26e42d6d61ff387
Vijay Rathor <vijay.rathor@mindruby.com>
2024-03-26 15:34:16 +0530

    Optimised intToRoman

--- leetcode/Question12.java
+++ leetcode/Question12.java
@@ -2,50 +2,50 @@ package leetcode;
 
 public class Question12 {
     public static String intToRoman(int num) {
-        String result = "";
+        StringBuilder result = new StringBuilder();
         while (num > 0) {
             if (num - 1000 >= 0) {
-                result += "M";
+                result.append("M");
                 num -= 1000;
             } else if (num - 900 >= 0) {
-                result += "CM";
+                result.append("CM");
                 num -= 900;
             } else if (num - 500 >= 0) {
-                result += "D";
+                result.append("D");
                 num -= 500;
             } else if (num - 400 >= 0) {
-                result += "CD";
+                result.append("CD");
                 num -= 400;
             } else if (num - 100 >= 0) {
-                result += "C";
+                result.append("C");
                 num -= 100;
             } else if (num - 90 >= 0) {
-                result += "XC";
+                result.append("XC");
                 num -= 90;
             } else if (num - 50 >= 0) {
-                result += "L";
+                result.append("L");
                 num -= 50;
             } else if (num - 40 >= 0) {
-                result += "XL";
+                result.append("XL");
                 num -= 40;
             } else if (num - 10 >= 0) {
-                result += "X";
+                result.append("X");
                 num -= 10;
             } else if (num - 9 >= 0) {
-                result += "IX";
+                result.append("IX");
                 num -= 9;
             } else if (num - 5 >= 0) {
-                result += "V";
+                result.append("V");
                 num -= 5;
             } else if (num - 4 >= 0) {
-                result += "IV";
+                result.append("IV");
                 num -= 4;
             } else if (num - 1 >= 0) {
-                result += "I";
+                result.append("I");
                 num -= 1;
             }
         }
-        return result;
+        return result.toString();
     }
 
     public static void main(String[] args) {
