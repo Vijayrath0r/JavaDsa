d03704789f87c9c35d6c3a771456672ec7d7f7c6
Vijay Rathor <vijay.rathor@mindruby.com>
2024-05-16 14:49:03 +0530

    solved not good Merge Intervals

--- leetcode/Question56.java
+++ leetcode/Question56.java
@@ -1,25 +1,23 @@
 // 56. Merge Intervals
 package leetcode;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.LinkedList;
 
 public class Question56 {
-    public static int[][] merge(int[][] intervals) {
-        List<int[]> validIntervals = new ArrayList<>();
-        int max = 0;
-        int min = 0;
-        for (int i = 0; i < intervals.length; i++) {
-            min = intervals[i][0];
-            max = intervals[i][1];
-            validIntervals.add(new int[] { min, max });
+    private static int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        LinkedList<int[]> merged = new LinkedList<>();
+
+        for (int[] interval : intervals) {
+            if (merged.isEmpty() || merged.getLast()[1] < interval[0]) {
+                merged.add(interval);
+            } else {
+                merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
+            }
         }
-        int[][] reducedIntervals = new int[validIntervals.size()][2];
-        for (int i = 0; i < validIntervals.size(); i++) {
-            reducedIntervals[i] = validIntervals.get(i);
-        }
-        return reducedIntervals;
+
+        return merged.toArray(new int[merged.size()][]);
     }
 
     public static void main(String[] args) {
