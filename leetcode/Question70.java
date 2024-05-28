06e7827e949914e4aefdc63dc47c78ff06d3d68b
Vijay rathor <vijay.rathor@mindruby.com>
2024-05-28 16:15:05 +0530

    solved Climbing Stairs at best

--- leetcode/Question70.java
+++ leetcode/Question70.java
@@ -1,20 +1,27 @@
 package leetcode;
 
+// Climbing Stairs
 public class Question70 {
-    public static int helper(int currentPos, int target) {
+    public static int helper(int currentPos, int target, int[] stairs) {
         if (currentPos > target || target < 1) {
             return 0;
         }
         if (currentPos == target) {
             return 1;
         }
-        int stepOne = helper(currentPos + 1, target);
-        int stepTwo = helper(currentPos + 2, target);
-        return stepOne + stepTwo;
+        if (stairs[currentPos] != 0) {
+            return stairs[currentPos];
+        }
+        int sum = helper(currentPos + 1, target, stairs) + helper(currentPos + 2, target, stairs);
+        if (stairs[currentPos] == 0 || stairs[currentPos] < sum) {
+            stairs[currentPos] = sum;
+        }
+        return sum;
     }
 
     public static int climbStairs(int n) {
-        return helper(0, n);
+        int[] stairs = new int[n];
+        return helper(0, n, stairs);
     }
 
     public static void main(String[] args) {
