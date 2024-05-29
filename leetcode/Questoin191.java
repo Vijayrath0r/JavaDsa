30c601644db5388d5a46ea061c0298199ff31622
Vijay rathor <vijay.rathor@mindruby.com>
2024-05-29 15:05:34 +0530

    solved Number of 1 Bits at best

---
+++ leetcode/Questoin191.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+// Number of 1 Bits
+public class Questoin191 {
+    private static int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            count += n & 1;
+            n >>= 1;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        // int n = 11;
+        int n = 128;
+        System.out.println(hammingWeight(n));
+    }
+}
