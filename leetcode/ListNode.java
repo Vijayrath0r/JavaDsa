e875f7fb63194efbaec18526c7953e8ea0ab04c8
Vijay rathor <vijay.rathor@mindruby.com>
2024-05-31 11:09:35 +0530

    solved Remove Duplicates from Sorted List at best

--- leetcode/ListNode.java
+++ leetcode/ListNode.java
@@ -15,4 +15,17 @@ public class ListNode {
         this.val = val;
         this.next = next;
     }
+
+    public void print() {
+        ListNode current = this; // Start with the current instance
+        System.out.println("************************************************************************");
+        System.out.println("| head");
+        System.out.println("V");
+        while (current != null) {
+            System.out.print(current.val + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+        System.out.println("************************************************************************");
+    }
 }
---
+++ leetcode/Question83.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+// Remove Duplicates from Sorted List
+public class Question83 {
+    private static ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(1);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = new ListNode(3);
+
+        ListNode result = deleteDuplicates(head);
+        result.print();
+    }
+}
