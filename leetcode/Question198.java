244cd44176cbc23dd43104cd3899f9ee4b9c84e4
Vijay rathor <vijay.rathor@mindruby.com>
2024-06-06 19:02:47 +0530

    solved House Robber at best

---
+++ leetcode/Question198.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+// House Robber
+import java.util.Arrays;
+
+public class Question198 {
+    private static int rob(int[] nums) {
+        int len = nums.length;
+        if (len == 1) {
+            return nums[0];
+        }
+        if (len == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        int[] dp = new int[len];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]); // Correctly initialize the second house
+
+        for (int i = 2; i < len; i++) {
+            dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]); // Correctly calculate dp[i]
+        }
+        return dp[len - 1];
+    }
+
+    public static void main(String[] args) {
+        // int[] nums = { 1, 2, 3, 1 };
+        // int[] nums = { 2, 7, 9, 3, 1 };
+        int[] nums = { 2, 1, 1, 2 };
+        System.out.println(rob(nums));
+    }
+}
