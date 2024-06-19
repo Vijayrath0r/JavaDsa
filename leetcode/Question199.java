eecc1d43320a0b2101ce7347dcb2996ff9766a91
Vijay rathor <vijay.rathor@mindruby.com>
2024-06-19 18:46:08 +0530

    solved  Binary Tree Right Side View at good using BFS.

--- leetcode/Question199.java
+++ leetcode/Question199.java
@@ -2,26 +2,43 @@ package leetcode;
 
 // Binary Tree Right Side View
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Question199 {
     private static List<Integer> rightSideView(TreeNode root) {
         List<Integer> result = new ArrayList<>();
-        while (root != null) {
-            result.add(root.val);
-            if (root.right != null) {
-                root = root.right;
-            } else if (root.left != null) {
-                root = root.left;
-            } else {
-                break;
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                // If it's the last node of the current level, add it to the result
+                if (i == levelSize - 1) {
+                    result.add(node.val);
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
             }
         }
+
         return result;
     }
 
     public static void main(String[] args) {
-        Integer[] tree = { 1, 2, 3, null, 5, null, 4 };
+        // Integer[] tree = { 1, 2, 3, null, 5, null, 4 };
+        Integer[] tree = { 1, 2, 3, 4 };
         TreeNode root = TreeNode.createTreeFromArray(tree);
 
         System.out.println(rightSideView(root));
