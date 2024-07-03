c8b6f2b8bbec68c88aedb3bcabc9d860f54b777b
Rohan Verma <vijay.rathor@mindruby.com>
2024-07-03 15:33:56 +0530

    solved Min Stack at best

---
+++ leetcode/Question155.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+// Min Stack
+import java.util.Stack;
+
+class MinStack {
+    private Stack<int[]> stack;
+
+    public MinStack() {
+        this.stack = new Stack<>();
+    }
+
+    public void push(int val) {
+        if (stack.isEmpty()) {
+            stack.push(new int[] { val, val });
+        } else {
+            stack.push(new int[] { val, Math.min(val, stack.peek()[1]) });
+        }
+    }
+
+    public void pop() {
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek()[0];
+    }
+
+    public int getMin() {
+        return stack.peek()[1];
+    }
+}
+
+public class Question155 {
+    public static void main(String[] args) {
+
+    }
+}
