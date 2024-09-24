5c19a733d46b45fcd838e68f5916b5b878fdee80
Vijay Rathor <vijay.rathor@mindruby.com>
2024-09-24 15:56:28 +0530

    solved GFG  Minimum Jumps

--- geeksforgeeks/MinimumJumps.java
+++ geeksforgeeks/MinimumJumps.java
@@ -1,31 +1,33 @@
 package geeksforgeeks;
 // Minimum Jumps
 
-import java.util.Arrays;
-
 public class MinimumJumps {
     static int minJumps(int[] arr) {
-        int len = arr.length;
-        int[] dp = new int[len];
-        Arrays.fill(dp, Integer.MAX_VALUE - 1);
-        dp[0] = 0;
+        int currentMax = 0;
+        int currentReach = 0;
+        int jumps = 0;
 
-        for (int i = 0; i < len; i++) {
-            if (arr[i] == 0) {
-                continue;
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] == 0 && currentMax <= i) {
+                return -1;
             }
-            for (int j = i + 1; j <= i + arr[i] && j < len; j++) {
-                dp[j] = Math.min(dp[j], dp[i] + 1);
+            currentMax = Math.max(currentMax, i + arr[i]);
+
+            if (i == currentReach) {
+                jumps++;
+                currentReach = currentMax;
             }
         }
-        return dp[len - 1] == Integer.MAX_VALUE - 1 ? -1 : dp[len - 1];
+
+        return arr.length > 1 && jumps == 0 ? -1 : jumps;
     }
 
     public static void main(String[] args) {
-        int arr[] = { 1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9 };
+        // int arr[] = { 1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9 };
         // int arr[] = { 1, 4, 3, 2, 6, 7 };
         // int arr[] = { 0, 10, 20 };
         // int arr[] = { 4, 3, 2, 1, 0, 1 };
+        int arr[] = { 9, 10, 1, 2, 3, 4, 8, 0, 0, 0, 0, 0, 0, 0, 1 };
         System.out.println(minJumps(arr));
     }
 }
