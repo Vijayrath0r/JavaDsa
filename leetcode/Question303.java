f457e3916f486090c2d6d142ff5e570a38dd7bd0
Vijay Rathor <vijay.rathor@mindruby.com>
2024-09-26 16:52:16 +0530

    solved 303. Range Sum Query - Immutable

---
+++ leetcode/Question303.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+// 303. Range Sum Query - Immutable
+public class Question303 {
+    private int[] prefixSum;
+
+    public Question303(int[] nums) {
+        prefixSum = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            prefixSum[i + 1] = prefixSum[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        return prefixSum[right + 1] - prefixSum[left];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { -2, 0, 3, -5, 2, -1 };
+        Question303 obj = new Question303(nums);
+
+        System.out.println(obj.sumRange(0, 2));
+        System.out.println(obj.sumRange(2, 5));
+        System.out.println(obj.sumRange(0, 5));
+    }
+}
